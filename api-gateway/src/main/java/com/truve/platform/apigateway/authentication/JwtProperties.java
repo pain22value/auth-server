a6758248144b56b45962e3ca018bdd0af1f146e7
l-lyun <kimdohyun032@gmail.com>
2026-02-04 17:28:06 +0900

    feat: JwtProperties 작성

---
+++ api-gateway/src/main/java/com/truve/platform/apigateway/authentication/JwtProperties.java
@@ -0,0 +1,30 @@
+package com.truve.platform.apigateway.authentication;
+
+import java.util.Date;
+
+import javax.crypto.SecretKey;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+import io.jsonwebtoken.security.Keys;
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+@ConfigurationProperties(prefix = "jwt")
+public class JwtProperties {
+	private final String secret;
+	private Long accessTokenExpiration;
+	private Long refreshTokenExpiration;
+
+	public Date getAccessTokenExpiration() {
+		return new Date(new Date().getTime() + accessTokenExpiration);
+	}
+
+	public Date refreshTokenExpiration() {
+		return new Date(new Date().getTime() + refreshTokenExpiration);
+	}
+
+	public SecretKey getSecretKey() {
+		return Keys.hmacShaKeyFor(secret.getBytes());
+	}
+}
